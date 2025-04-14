a079980ffeba816fcbcacd619d3d712afe7802b4
glidea <740696441@qq.com>
2025-04-14 22:53:14 +0800

    adapt cf

---
+++ svelte.config.js
@@ -0,0 +1,22 @@
+// svelte.config.js
+import adapter from '@sveltejs/adapter-cloudflare';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+    // Consult https://kit.svelte.dev/docs/integrations#preprocessors
+    // for more information about preprocessors
+    preprocess: vitePreprocess(),
+
+    kit: {
+        adapter: adapter({
+            // See options: https://github.com/sveltejs/kit/tree/main/packages/adapter-cloudflare#options
+            routes: {
+                include: ['/*'],
+                exclude: ['<all>']
+            }
+        })
+    }
+};
+
+export default config;
